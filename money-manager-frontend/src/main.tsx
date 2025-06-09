2bd3f0f3426d0b86d6680fe6f2bc32bc9fe403ae
Jairo Júnior <jairojjr2003@gmail.com>
2025-06-08 22:43:37 -0300

    feat: starting the Sign In view

--- money-manager-frontend/src/main.tsx
+++ money-manager-frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './utils/index.css'
+import SignIn from './pages/SignIn'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SignIn />
   </StrictMode>,
 )
