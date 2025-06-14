deebb3a6a729754e1684bfe5533377a9eec92849
Jairo Júnior <jairojjr2003@gmail.com>
2025-06-13 21:20:00 -0300

    feat: modifying the background

--- money-manager-frontend/src/pages/SignIn/components/background.tsx
+++ money-manager-frontend/src/pages/SignIn/components/background.tsx
@@ -13,16 +13,7 @@ function Background() {
         <div className="symbol symbol-plusminus">±</div>
         <div className="symbol symbol-infinity">∞</div>
        
-        {/* Chart-like lines */}
-        <svg className="fullscreen-background" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <pattern id="grid" width="50" height="50" patternUnits="userSpaceOnUse">
-              <path d="M 50 0 L 0 0 0 50" fill="none" stroke="rgba(255,255,255,0.05)" strokeWidth="1"/>
-            </pattern>
-          </defs>
-          <rect width="100%" height="100%" fill="url(#grid)" />
-          
-        </svg>
+     
     </div>
   );
 }
